c803b1c69f913bb57aafe2b01eb437ada39b5b92
Tirsog <tirsog@gmail.com>
2022-12-23 17:00:04 +0000

    removed contact import

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
-import Contactblock from "../components/contactblock/contactblock"
 
 export default function Home() {
     return (
