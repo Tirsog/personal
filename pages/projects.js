9299fbb92fea3c4024cce3e9eafef0272fdf1309
Tirsog <tirsog@gmail.com>
2023-04-05 10:23:00 +0100

    Testing new form

--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,40 @@
 import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Projects.module.css"
-import Navbar from "../components/navbar"
+import { useState } from "react"
 
 export default function Home() {
+    const [fullname, setFullname] = useState("")
+    const [email, setEmail] = useState("")
+    const [subject, setSubject] = useState("")
+    const [message, setMessage] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        /*      let isValidForm = handleValidation() */
+
+        const res = await fetch("/api/sendgrid", {
+            body: JSON.stringify({
+                email: email,
+                fullname: fullname,
+                subject: subject,
+                message: message,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+        })
+
+        const { error } = await res.json()
+        if (error) {
+            console.log(error)
+            return
+        }
+        console.log(fullname, email, subject, message)
+    }
+
     return (
         <div className={styles.container}>
             <Head>
@@ -14,57 +45,103 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Projects</a>
-                </h1>
+            <main>
+                <form
+                    onSubmit={handleSubmit}
+                    className="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-blue-500"
+                >
+                    <h1 className="text-2xl font-bold dark:text-gray-50">
+                        Send a message
+                    </h1>
 
-                <p className={styles.description}>
-                    Get started by editing{" "}
-                    <code className={styles.code}>pages/index.js</code>
-                </p>
-
-                <div className={styles.grid}>
-                    <a href="https://nextjs.org/docs" className={styles.card}>
-                        <h2>Documentation &rarr;</h2>
-                        <p>
-                            Find in-depth information about Next.js features and
-                            API.
-                        </p>
-                    </a>
-
-                    <a href="https://nextjs.org/learn" className={styles.card}>
-                        <h2>Learn &rarr;</h2>
-                        <p>
-                            Learn about Next.js in an interactive course with
-                            quizzes!
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://github.com/vercel/next.js/tree/canary/examples"
-                        className={styles.card}
+                    <label
+                        htmlFor="fullname"
+                        className="text-gray-500 font-light mt-8 dark:text-gray-50"
                     >
-                        <h2>Examples &rarr;</h2>
-                        <p>
-                            Discover and deploy boilerplate example Next.js
-                            projects.
-                        </p>
-                    </a>
+                        Full name
+                        <span className="text-red-500 dark:text-gray-50">
+                            *
+                        </span>
+                    </label>
+                    <input
+                        type="text"
+                        value={fullname}
+                        onChange={(e) => {
+                            setFullname(e.target.value)
+                        }}
+                        name="fullname"
+                        className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
+                    />
 
-                    <a
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={styles.card}
+                    <label
+                        htmlFor="email"
+                        className="text-gray-500 font-light mt-4 dark:text-gray-50"
                     >
-                        <h2>Deploy &rarr;</h2>
-                        <p>
-                            Instantly deploy your Next.js site to a public URL
-                            with Vercel.
-                        </p>
-                    </a>
-                </div>
+                        E-mail<span className="text-red-500">*</span>
+                    </label>
+                    <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value)
+                        }}
+                        className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
+                    />
+
+                    <label
+                        htmlFor="subject"
+                        className="text-gray-500 font-light mt-4 dark:text-gray-50"
+                    >
+                        Subject<span className="text-red-500">*</span>
+                    </label>
+                    <input
+                        type="text"
+                        name="subject"
+                        value={subject}
+                        onChange={(e) => {
+                            setSubject(e.target.value)
+                        }}
+                        className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
+                    />
+
+                    <label
+                        htmlFor="message"
+                        className="text-gray-500 font-light mt-4 dark:text-gray-50"
+                    >
+                        Message<span className="text-red-500">*</span>
+                    </label>
+                    <textarea
+                        name="message"
+                        value={message}
+                        onChange={(e) => {
+                            setMessage(e.target.value)
+                        }}
+                        className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
+                    ></textarea>
+
+                    <div className="flex flex-row items-center justify-start">
+                        <button
+                            type="submit"
+                            className="px-10 mt-8 py-2 bg-[#130F49] text-gray-50 font-light rounded-md text-lg flex flex-row items-center"
+                        >
+                            Submit
+                            <svg
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                className="text-cyan-500 ml-2"
+                                fill="currentColor"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M9.00967 5.12761H11.0097C12.1142 5.12761 13.468 5.89682 14.0335 6.8457L16.5089 11H21.0097C21.562 11 22.0097 11.4477 22.0097 12C22.0097 12.5523 21.562 13 21.0097 13H16.4138L13.9383 17.1543C13.3729 18.1032 12.0191 18.8724 10.9145 18.8724H8.91454L12.4138 13H5.42485L3.99036 15.4529H1.99036L4.00967 12L4.00967 11.967L2.00967 8.54712H4.00967L5.44417 11H12.5089L9.00967 5.12761Z"
+                                    fill="currentColor"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                </form>
             </main>
         </div>
     )
