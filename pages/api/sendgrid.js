fc13c67d7597e2c49628263d7fc39379052a44f9
Tirsog <tirsog@gmail.com>
2023-04-06 12:07:46 +0100

    Updated emails

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -6,8 +6,8 @@ async function sendEmail(req, res) {
     try {
         // console.log("REQ.BODY", req.body);
         await sendgrid.send({
-            to: "contacto@tirsog.es", // Your email where you'll receive emails
-            from: "contacto@tirsog.es", // your website email address here
+            to: "hi@tirsog.es", // Your email where you'll receive emails
+            from: "hi@tirsog.es", // your website email address here
             subject: `Contact Form: ${req.body.subject}`,
             html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
       <html lang="en">
